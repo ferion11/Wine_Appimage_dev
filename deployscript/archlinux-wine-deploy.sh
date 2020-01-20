5fa68b2ad9b66d1bd4de41c794ce1645b9bac3b0
DanielDevBR <dados.linux@gmail.com>
2020-01-20 11:50:05 -0300

    add wine-test2 for variations test

--- deployscript/archlinux-wine-deploy.sh
+++ deployscript/archlinux-wine-deploy.sh
@@ -263,7 +263,14 @@ rm src/{libhookexecv.so,wine-preloader_hook}
 
 cp AppRun $WINE_WORKDIR
 cp resource/* $WINE_WORKDIR
+#-----------------------------
+
+#test for others AppImage variations:
+cp -rp $WINE_WORKDIR test2
+mkdir test2/mark_test2
+#-----------------------------
 
 ./appimagetool.AppImage --appimage-extract
 
 export ARCH=x86_64; squashfs-root/AppRun -v $WINE_WORKDIR -u 'gh-releases-zsync|ferion11|Wine_Appimage|continuous|wine-i386*arch*.AppImage.zsync' wine-i386_${ARCH}-archlinux.AppImage
+export ARCH=x86_64; squashfs-root/AppRun -v test2 -u 'gh-releases-zsync|ferion11|Wine_Appimage|continuous|wine-test2-i386*arch*.AppImage.zsync' wine-test2-i386_${ARCH}-archlinux.AppImage
