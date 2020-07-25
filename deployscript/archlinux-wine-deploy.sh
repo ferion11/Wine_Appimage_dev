e19c7571c3613985e491368bfbceaf754a7c6619
DanielDevBR <dados.linux@gmail.com>
2020-07-25 20:29:37 -0300

    trying to disable the chaotic-aur mirror

--- deployscript/archlinux-wine-deploy.sh
+++ deployscript/archlinux-wine-deploy.sh
@@ -111,15 +111,17 @@ echo "SigLevel = Optional TrustAll" >> /etc/pacman.conf
 echo "Server = https://repo.archlinuxcn.org/\$arch" >> /etc/pacman.conf
 echo "" >> /etc/pacman.conf
 
-# https://lonewolf.pedrohlc.com/chaotic-aur/
-echo "[chaotic-aur]" >> /etc/pacman.conf
-echo "SigLevel = Optional TrustAll" >> /etc/pacman.conf
-echo "Server = http://lonewolf-builder.duckdns.org/\$repo/x86_64" >> /etc/pacman.conf
-echo "Server = http://chaotic.bangl.de/\$repo/x86_64" >> /etc/pacman.conf
-echo "Server = https://repo.kitsuna.net/x86_64" >> /etc/pacman.conf
-echo "" >> /etc/pacman.conf
-#pacman-key --keyserver keys.mozilla.org -r 3056513887B78AEB
-#pacman-key --lsign-key 3056513887B78AEB
+## https://lonewolf.pedrohlc.com/chaotic-aur/
+#echo "[chaotic-aur]" >> /etc/pacman.conf
+#echo "SigLevel = Optional TrustAll" >> /etc/pacman.conf
+#echo "Server = http://lonewolf-builder.duckdns.org/\$repo/x86_64" >> /etc/pacman.conf
+#echo "Server = http://chaotic.bangl.de/\$repo/x86_64" >> /etc/pacman.conf
+#echo "Server = https://repo.kitsuna.net/x86_64" >> /etc/pacman.conf
+#echo "" >> /etc/pacman.conf
+##pacman-key --keyserver keys.mozilla.org -r 3056513887B78AEB
+##pacman-key --lsign-key 3056513887B78AEB
+##sudo pacman-key --keyserver hkp://p80.pool.sks-keyservers.net:80 -r 3056513887B78AEB
+##sudo pacman-key --lsign-key 3056513887B78AEB
 
 # workaround one bug: https://bugzilla.redhat.com/show_bug.cgi?id=1773148
 echo "Set disable_coredump false" >> /etc/sudo.conf
