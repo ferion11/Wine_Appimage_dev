37d5ed5195425217c7e3631a7a8eec4388f41387
DanielDevBR <dados.linux@gmail.com>
2021-03-26 17:21:47 -0300

    reworked the basic deploy

--- deploy.sh
+++ deploy.sh
@@ -21,21 +21,22 @@ echo "P_VERSION: ${P_VERSION}"
 # ${P_NAME}-${P_MVERSION}-v${P_VERSION}-${P_CSOURCE}-x86_64.AppImage
 echo "RESULT: ${P_NAME}-${P_MVERSION}-v${P_VERSION}-${P_CSOURCE}-x86_64.AppImage"
 
-wget -nv -c "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage" -O  appimagetool.AppImage
-mv appimagetool.AppImage "${P_NAME}-${P_MVERSION}-v${P_VERSION}-${P_CSOURCE}-x86_64.AppImage"
-echo "Content" > "${P_NAME}-${P_MVERSION}-v${P_VERSION}-${P_CSOURCE}-x86_64.AppImage.zsync"
-exit 0
-# DEBUG test only
-#??????????????????????????????????????????????????????????????????????????????
+# test debug only:
+#wget -nv -c "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage" -O  appimagetool.AppImage
+#mv appimagetool.AppImage "${P_NAME}-${P_MVERSION}-v${P_VERSION}-${P_CSOURCE}-x86_64.AppImage"
+#echo "Content" > "${P_NAME}-${P_MVERSION}-v${P_VERSION}-${P_CSOURCE}-x86_64.AppImage.zsync"
+#exit 0
+# end test debug
 
 #=========================
 die() { echo >&2 "$*"; exit 1; };
 #=========================
 
-#Add "base-devel multilib-devel" for compile in the list:
-pacman -S --noconfirm wget git tar grep sed zstd xz bzip2 || die "ERROR: Some packages not found! to run the script!!!"
+# Pre install
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install -y aptitude wget file bzip2 patchelf || die "ERROR: Some packages not found! to run the script!!!"
 #===========================================================================================
-
 #-----------------------------------
 
 # Get Wine
@@ -46,36 +47,15 @@ tar xf $P_FILENAME -C "$WINE_WORKDIR"/
 
 cd "$WINE_WORKDIR" || die "ERROR: Directory don't exist: $WINE_WORKDIR"
 
-# Add a dependency library, such as freetype font library
-dependencys=$(pactree -s -u wine |grep lib32 | xargs)
 
-mkdir cache
-#mv *.pkg.tar* ./cache/ || die "ERROR: None package builded from AUR"
-mv *.pkg.tar* ./cache/ || echo "INFO: None package builded from AUR"
+pkgcachedir='/tmp/.pkgcachedir'
+mkdir -p "${pkgcachedir}"
 
-pacman -Scc --noconfirm
-pacman -Syw --noconfirm --cachedir cache lib32-alsa-lib lib32-alsa-plugins lib32-faudio lib32-fontconfig lib32-freetype2 lib32-gcc-libs lib32-gettext lib32-giflib lib32-glu lib32-gnutls lib32-gst-plugins-base lib32-lcms2 lib32-libjpeg-turbo lib32-libjpeg6-turbo lib32-libldap lib32-libpcap lib32-libpng lib32-libpng12 lib32-libsm lib32-libxcomposite lib32-libxcursor lib32-libxdamage lib32-libxi lib32-libxml2 lib32-libxmu lib32-libxrandr lib32-libxslt lib32-libxxf86vm lib32-mesa lib32-mesa-libgl lib32-mpg123 lib32-ncurses lib32-openal lib32-sdl2 lib32-v4l-utils lib32-libdrm lib32-libva lib32-krb5 lib32-flac lib32-gst-plugins-good lib32-libcups lib32-libwebp lib32-libvpx lib32-libvpx1.3 lib32-portaudio lib32-sdl lib32-sdl2_image lib32-sdl2_mixer lib32-sdl2_ttf lib32-sdl_image lib32-sdl_mixer lib32-sdl_ttf lib32-smpeg lib32-speex lib32-speexdsp lib32-twolame lib32-ladspa lib32-libao lib32-libvdpau lib32-libpulse lib32-libcanberra-pulse lib32-libcanberra-gstreamer lib32-glew lib32-mesa-demos lib32-jansson lib32-libxinerama lib32-atk lib32-vulkan-icd-loader lib32-vulkan-intel lib32-vulkan-radeon lib32-vkd3d lib32-aom lib32-gsm lib32-lame lib32-libass lib32-libbluray lib32-dav1d lib32-libomxil-bellagio lib32-x264 lib32-x265 lib32-xvidcore lib32-opencore-amr lib32-openjpeg2 lib32-ncurses5-compat-libs $dependencys || die "ERROR: Some packages not found!!!"
+aptitude -y -d -o dir::cache::archives="${pkgcachedir}" install libwine:i386 libva2:i386 libva-drm2:i386 libva-x11-2:i386 libvulkan1:i386
 
-# Remove non lib32 pkgs before extracting
-#echo "All files in ./cache: $(ls ./cache)"
-find ./cache -type f ! -name "lib32*" -exec rm {} \; -exec echo "Removing: {}" \;
-#find ./cache -type f -name "*x86_64*" -exec rm {} \; -exec echo "Removing: {}" \; #don't work because the name of lib32 multilib packages have the x86_64 too
-echo "DEBUG: clean some packages"
-rm -rf ./cache/lib32-clang*
-rm -rf ./cache/lib32-nvidia-cg-toolkit*
-rm -rf ./cache/lib32-ocl-icd*
-rm -rf ./cache/lib32-opencl-mesa*
-echo "All files in ./cache: $(ls ./cache)"
+find $pkgcachedir -name '*deb' ! -name 'libwine*' -exec dpkg -x {} . \;
 
-
-# extracting *tar.xz...
-find ./cache -name '*tar.xz' -exec tar --warning=no-unknown-keyword -xJf {} \;
-
-# extracting *tar.zst...
-find ./cache -name '*tar.zst' -exec tar --warning=no-unknown-keyword --zstd -xf {} \;
-
-# extracting *tar...
-find ./cache -name '*tar' -exec tar --warning=no-unknown-keyword -xf {} \;
+rm -rf $pkgcachedir
 
 #----------------------------------------------
 
